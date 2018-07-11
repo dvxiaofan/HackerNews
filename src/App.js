ad2baba023d8f09ebef381a61c2806bbf4ecfda4
xiaofan <1353815633@qq.com>
2018-07-11 15:27:52 +0800

    state

--- src/App.js
+++ src/App.js
@@ -22,10 +22,19 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item => (
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -33,7 +42,7 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
           </div>
-        ))}
+        )}
       </div>
     );
   }
