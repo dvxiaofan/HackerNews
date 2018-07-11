3ab406a387478532099c4199e5103e5a445d219c
xiaofan <1353815633@qq.com>
2018-07-11 17:19:42 +0800

    实现简单搜索功能

--- src/App.js
+++ src/App.js
@@ -21,16 +21,30 @@ const list = [
   }
 ];
 
+/**
+ * 定义一个高阶函数
+ * ES6 箭头函数优化
+ * @param {*} searchTerm
+ * @returns
+ */
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      list: list
+      list,
+      searchTerm: '',
     }
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id) {
@@ -41,7 +55,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item => 
+        <form>
+          <input
+            type='text' 
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
