dcb87b7cea96ff9cae391238ba24a1ec611118fd
xiaofan <1353815633@qq.com>
2018-07-12 12:45:13 +0800

    设置组件样式

--- src/App.js
+++ src/App.js
@@ -55,47 +55,6 @@ const isSearched = searchTerm =>
   item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-const Button = ({ onClick, className, children }) =>
-  <button
-    type='button'
-    onClick={onClick}
-    className={className}
-  >
-    {children}
-  </button>
-
-// 函数式无状态组件
-const Search = ({ value, onChange, children }) => 
-  <form>
-    {children}
-    <input
-      type='text'
-      value={value}
-      onChange={onChange}
-    />
-  </form>
-
-const Table = ({ list, pattern, onDismiss }) => 
-  <div>
-    {list.filter(isSearched(pattern)).map(item => 
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author} </span>
-        <span>{item.num_comments} </span>
-        <span>{item.points} </span>
-        <span>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-          >
-            Dismiss
-          </Button>
-        </span>
-      </div>
-    )}
-  </div>
-
 class App extends Component {
 
   constructor(props) {
@@ -126,13 +85,14 @@ class App extends Component {
       searchTerm
     } = this.state;
     return (
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-        搜索：
-        </Search>
+      <div className="page">
+        <div className='interactions'>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+          </Search>
+        </div>
         <Table
           list={list}
           pattern={searchTerm}
@@ -142,5 +102,59 @@ class App extends Component {
     );
   }
 }
+
+const Button = ({ onClick, className, children }) =>
+  <button
+    type='button'
+    onClick={onClick}
+    className={className}
+  >
+    {children}
+  </button>
+
+// 函数式无状态组件
+const Search = ({ value, onChange, children }) => 
+  <form>
+    <input
+      type='text'
+      value={value}
+      onChange={onChange}
+    />
+    <button type='submit'>Search</button>
+  </form>
+
+// 定义三个自适应宽度
+const largeColumn = {
+  width: '40%',
+};
+const midColumn = {
+  width: '30%',
+};
+const smallColumn = {
+  width: '10%',
+};
+
+const Table = ({ list, pattern, onDismiss }) => 
+  <div className='table'>
+    {list.filter(isSearched(pattern)).map(item => 
+      <div key={item.objectID} className='table-row'>
+        {/* 使用内联样式来使 Table 的列宽自适应 */}
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>{item.author} </span>
+        <span style={smallColumn}>{item.num_comments} </span>
+        <span style={smallColumn}>{item.points} </span>
+        <span style={smallColumn}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className='button-inline'
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
 
 export default App;
