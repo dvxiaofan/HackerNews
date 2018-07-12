fd11170ad0dbf47f001a7f400e80cefae7767120
xiaofan <1353815633@qq.com>
2018-07-12 11:10:24 +0800

    利用ES6进行状态解构

--- src/App.js
+++ src/App.js
@@ -77,6 +77,11 @@ class App extends Component {
   }
 
   render() {
+    // 状态解构
+    const {
+      list,
+      searchTerm
+    } = this.state;
     return (
       <div className="App">
         <form>
@@ -85,7 +90,7 @@ class App extends Component {
             onChange={this.onSearchChange}
           />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
+        {list.filter(isSearched(searchTerm)).map(item => 
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
