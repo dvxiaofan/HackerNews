383239e505c477bd68ffca716528fe77beb9a5ab
xiaofan <1353815633@qq.com>
2018-07-12 11:33:51 +0800

    拆分组件

--- src/App.js
+++ src/App.js
@@ -53,6 +53,49 @@ const list = [
  */
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+
+    return (
+      <form>
+        <input
+          type='text'
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => 
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.points} </span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type='button'
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
 class App extends Component {
 
   constructor(props) {
@@ -84,32 +127,16 @@ class App extends Component {
     } = this.state;
     return (
       <div className="App">
-        <form>
-          <input
-            type='text'
-            // input 设置value属性， 变为可控组件
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item => 
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author} </span>
-            <span>{item.num_comments} </span>
-            <span>{item.points} </span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type='button'
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        )}
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
