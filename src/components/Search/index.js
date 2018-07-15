1afaa9958bbc1600db56e6a75d5fd0b4de24e417
xiaofan <1353815633@qq.com>
2018-07-15 17:42:01 +0800

    引用DOM元素， 使input 聚焦

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// 函数式无状态组件
-const Search = ({
-  value,
-  onSubmit,
-  onChange,
-  children
-}) => (
-  <form onSubmit={onSubmit}>
-    <input
-      type='text'
-      value={value}
-      onChange={onChange}
-    />
-    <button type='submit'>
-      {children}
-    </button>
-  </form>
-)
+class Search extends Component {
+
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const {
+      value,
+      onSubmit,
+      onChange,
+      children
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={onChange}
+          ref={(node) => { this.input = node; }}
+        />
+        <button type='submit'>
+          {children}
+        </button>
+      </form>
+    )
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
